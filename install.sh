b638354a972891d1f842d0ca318e77b3a76bbb28
jsp1256 <googg@jsp1256.uu.me>
2018-06-16 23:51:06 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 ##install.sh
-##version:1.0
+##version:1.1
 #获取当前的绝对路径
 basepath=$(cd `dirname $0`; pwd)
 #package的绝对路径
@@ -28,3 +28,7 @@ ln -s -f $INSTALL_LIBPATH/libevent_pthreads-2.1.so.6.0.2 $INSTALL_LIBPATH/libeve
 ln -s -f $INSTALL_LIBPATH/libssl.so.1.1 $INSTALL_LIBPATH/libssl.so.1
 ln -s -f $INSTALL_LIBPATH/libz.so.1.2.11 $INSTALL_LIBPATH/libz.so.1
 ln -s -f $INSTALL_LIBPATH/libz.so.1.2.11 $INSTALL_LIBPATH/libz.so
+#修改系统内核配置文件，解决UDP转发缓存区设置失败
+echo \#transmission add >> /etc/sysctl.conf
+echo net.core.rmem_max = 4194304 >> /etc/sysctl.conf
+echo net.core.wmem_max = 1048576 >> /etc/sysctl.conf
