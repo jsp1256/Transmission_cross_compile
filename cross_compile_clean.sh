32ce7f83b4d1590831e26401fa3b54d8f85ff99e
jsp1256 <googg@jsp1256.uu.me>
2018-06-17 10:31:26 +0800

    Update cross_compile_clean.sh

--- cross_compile_clean.sh
+++ cross_compile_clean.sh
@@ -1,6 +1,12 @@
 ##cross_compile_clean.sh
-##version:1.1
+##version:1.2
 USER=`whoami`
+if [ $USER = root ]
+then
+    USER_ROOT=""/$USER/
+else
+    USER_ROOT="/home/"$USER
+fi
 #配置全局位置变量
 basepath=$(cd `dirname $0`; pwd)
 BUILD_ROOT=$basepath/build/
@@ -9,6 +15,6 @@ CROSS_COMPILER_STAGING_DIR=$BUILD_ROOT/PandoraBoxSDK/staging_dir/
 CROSS_COMPILER_ROOT=$CROSS_COMPILER_STAGING_DIR/toolchain-mipsel_24kec+dsp_gcc-5.4.0_uClibc-1.0.x/bin/
 rm -rf $BUILD_ROOT
 #清除环境变量
-sed -i 's/export PATH=\$PATH:$CROSS_COMPILER_ROOT//g' .bashrc
-sed -i 's/export STAGING_DIR=$CROSS_COMPILER_STAGING_DIR//g' .bashrc
+sed -i 's/export PATH=\$PATH:$CROSS_COMPILER_ROOT//g' $USER_ROOT/.bashrc
+sed -i 's/export STAGING_DIR=$CROSS_COMPILER_STAGING_DIR//g' $USER_ROOT/.bashrc
 echo 脚本执行完毕
